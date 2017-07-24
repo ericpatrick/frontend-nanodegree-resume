4680f2fa841034d74575a7b5b5a3d592b562c0d4
Eric Patrick <ericpatrick15@gmail.com>
2017-07-24 19:14:53 -0300

    Curriculum data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,93 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+  name: "Eric Patrick",
+  role: "Web Developer",
+  contacts: {
+    email: "ericpatrick15@gmail.com",
+    github: "ericpatrick",
+    location: "Goiânia-GO"
+  },
+  pictureUrl: "./images/fry.jpg",
+  welcomeMessage: "Welcome",
+  skills: ["HTML", "CSS", "javascript", "LBS", "API"]
+};
+
+var work = {
+  position: "Fronted Developer",
+  employer: "GoGeo",
+  years: "3"
+};
+
+var education = {
+  schools: [
+    {
+      name: "Federal University of Goiás",
+      location: "Goiânia-GO, BR",
+      degree: "BA",
+      dates: "2008-2010",
+      url: "http://inf.ufg.br"
+    }
+  ],
+  onlineCourses: [
+    {
+      title: "Nanodegree Frontend",
+      school: "Udacity",
+      dates: 2018,
+      url: "http://udacity.com"
+    }
+  ]
+}
+
+var work = {
+  jobs: [
+    {
+      employer: "Laboratory for Ubiquitous and Pervasive Applications (LUPA) - UFG",
+      title: "Software Developer",
+      date: "2011 - 2012",
+      description: "Development of a geoAnalytics application that enable the user join many geospatial data and apply many geospatial operations"
+    },
+    {
+      employer: "CUIA Internet Brasil",
+      title: "Software Developer",
+      date: "2012-2013",
+      description: "Development of a tracking vehicles application" 
+    },
+    {
+      employer: "GoGeo",
+      title: "Frontend Developer",
+      date: "2013 - now",
+      description: "Development of a web application to manage the sellers and the logistic of distributors"
+    }
+  ]
+};
+
+var projects = {
+  projects: [
+    {
+      title: "SIGMA - Sistema de Informação Geográfico para Monitoramento Ambiental",
+      dates: "2011-2012",
+      description: "Geographic information system (GIS) that allows the user to select geographic data and perform crossings and space operations in order to perform analyzes of a given space",
+      images: []
+    },
+    {
+      title: "CarConnect",
+      dates: "2012-2013",
+      description: "Vehicle tracking system that allows monitoring, blocking and reporting of the vehicle being tracked",
+      images: []
+    },
+    {
+      title: "GoGeo Platform",
+      dates: "2013-2014",
+      description: "Geospatial services platform that allows the construction of geospatial applications through a service interface (API)",
+      images: []
+    },
+    {
+      title: "GeoBusiness",
+      dates: "2014-now",
+      description: "Application of management of sales territories to meet the needs of distributors and industries",
+      images: []
+    }
+  ]
+};
